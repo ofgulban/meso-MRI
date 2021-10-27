41381b0ea3ef925b5a074a89e16ee2bf059e9968
Omer Faruk Gulban <farukgulban@gmail.com>
2021-10-27 19:49:48 +0200

    wip

--- scripts/wip/flattening_explanation/01_render_360_folded.py
+++ scripts/wip/flattening_explanation/01_render_360_folded.py
@@ -5,15 +5,13 @@ import os
 import numpy as np
 import pyvista as pv
 import nibabel as nb
+from matplotlib.colors import ListedColormap
 
-FILENAME = "/home/faruk/data2/DATA_MRI_NIFTI/derived/sub-04/flattening/sub-04_ses-T2s_segm_rim_HG_RH_v02_borderized_multilaterate_perimeter_chunk_curvature_binned_flat_400x400_voronoi.nii.gz"
-OUTDIR = "/home/faruk/gdrive/paper-350_micron/paper_figures/results-patch_flatten_explain/flat_chunk"
-
-# Mask to smooth cake sides
-MASK = "/home/faruk/data2/DATA_MRI_NIFTI/derived/sub-04/median_maps/sub-04_ses-T2s_segm_rim_HG_RH_v02_borderized_multilaterate_perimeter_chunk_curvature_binned_flat_400x400_voronoi_median_projection.nii.gz"
+FILENAME = "/home/faruk/gdrive/paper-350_micron/paper_figures/results-patch_flatten_explain/sub-04_ses-T2s_segm_rim_HG_RH_v02_borderized_curvature_binned_masked_4visual_zoomed.nii.gz"
+OUTDIR = "/home/faruk/gdrive/paper-350_micron/paper_figures/results-patch_flatten_explain/folded_curvature"
 
 # Data range
-MIN, MAX = 0, 2
+MIN, MAX = 0, 2.1
 NR_FRAMES = 24
 BACKGROUND = "black"
 RESOLUTION = (1920, 1920)
@@ -30,16 +28,8 @@ if not os.path.exists(OUTDIR):
 nii = nb.load(FILENAME)
 data = nii.get_fdata()
 
-# Smooth sides
-mask = np.squeeze(nb.load(MASK).get_fdata())
-idx = mask != 0
-data[~(idx), :] = 0
-
 # Mirror data
-# data = data[::-1, :, :]
-
-# Fix top layer not being rendered
-data[:, :, -1] = 0
+data = data[::-1, :, :]
 
 # Normalize to 0-255
 data[data > MAX] = MAX
@@ -47,19 +37,28 @@ data = (data - MIN) / (MAX - MIN)
 data[data < 0] = 0
 data *= 255
 
+# Colors
+cmap_custom = np.ones((256, 4))
+cmap_custom[:, 0] = np.linspace(0, 1, 256)
+cmap_custom[:, 1] = np.linspace(0, 1, 256)
+cmap_custom[:, 2] = np.linspace(0, 1, 256)
+cmap_custom[-1, :] = [1, 0, 0, 1]
+cmap_custom = ListedColormap(cmap_custom)
+
 # Prep plotter
 p = pv.Plotter(window_size=RESOLUTION, off_screen=True)
 opacity = np.ones(255)
 opacity[0] = 0
-p.add_volume(data, cmap="gray", opacity=opacity, blending="composite",
+p.add_volume(data, cmap=cmap_custom, opacity=opacity, blending="composite",
              opacity_unit_distance=0, ambient=0, shade=True,
-             specular=0, specular_power=0)
+             specular=1, specular_power=128, resolution=[1.5, 1.5, 1.5],
+             culling=True)
 p.set_background(BACKGROUND)
 p.remove_scalar_bar()
 
 # -----------------------------------------------------------------------------
 # Manipulate camera
-p.camera.elevation += 10
+p.camera.elevation -= 10
 p.camera.zoom(1.3)
 for i in range(NR_FRAMES):
     sys.stdout.write("  Frame {}/{} \r".format(i+1, NR_FRAMES))
--- scripts/wip/flattening_explanation/02_render_360_flat.py
+++ scripts/wip/flattening_explanation/02_render_360_flat.py
@@ -6,8 +6,11 @@ import numpy as np
 import pyvista as pv
 import nibabel as nb
 
-FILENAME = "/home/faruk/gdrive/paper-350_micron/paper_figures/results-patch_flatten_explain/sub-04_ses-T2s_segm_rim_HG_RH_v02_borderized_curvature_binned_masked_zoomed.nii.gz"
-OUTDIR = "/home/faruk/gdrive/paper-350_micron/paper_figures/results-patch_flatten_explain/brain_chunk"
+FILENAME = "/home/faruk/data2/DATA_MRI_NIFTI/derived/sub-demo_flatten/sub-04_ses-T2s_segm_rim_HG_RH_v02_borderized_multilaterate_perimeter_chunk_curvature_binned_flat_400x400_voronoi_median_boxv3_5-5-11.nii.gz"
+OUTDIR = "/home/faruk/gdrive/paper-350_micron/paper_figures/results-patch_flatten_explain/flat_curvature"
+
+# Mask to smooth cake sides
+MASK = "/home/faruk/data2/DATA_MRI_NIFTI/derived/sub-04/median_maps/sub-04_ses-T2s_segm_rim_HG_RH_v02_borderized_multilaterate_perimeter_chunk_curvature_binned_flat_400x400_voronoi_median_projection.nii.gz"
 
 # Data range
 MIN, MAX = 0, 2
@@ -27,8 +30,16 @@ if not os.path.exists(OUTDIR):
 nii = nb.load(FILENAME)
 data = nii.get_fdata()
 
+# Smooth sides
+mask = np.squeeze(nb.load(MASK).get_fdata())
+idx = mask != 0
+data[~(idx), :] = 0
+
 # Mirror data
-data = data[::-1, :, :]
+# data = data[::-1, :, :]
+
+# Fix top layer not being rendered
+data[:, :, -1] = 0
 
 # Normalize to 0-255
 data[data > MAX] = MAX
@@ -40,16 +51,15 @@ data *= 255
 p = pv.Plotter(window_size=RESOLUTION, off_screen=True)
 opacity = np.ones(255)
 opacity[0] = 0
-p.add_volume(data, cmap="gray", opacity=opacity, blending="composite",
+p.add_volume(data, cmap=CMAP, opacity=opacity, blending="composite",
              opacity_unit_distance=0, ambient=0, shade=True,
-             specular=1, specular_power=128, resolution=[1.5, 1.5, 1.5],
-             culling=True)
+             specular=0, specular_power=0)
 p.set_background(BACKGROUND)
 p.remove_scalar_bar()
 
 # -----------------------------------------------------------------------------
 # Manipulate camera
-p.camera.elevation -= 10
+p.camera.elevation += 10
 p.camera.zoom(1.3)
 for i in range(NR_FRAMES):
     sys.stdout.write("  Frame {}/{} \r".format(i+1, NR_FRAMES))
