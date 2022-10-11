06283291c8deab032c62763aa3a02d8b0dbd8ac7
Omer Faruk Gulban <farukgulban@gmail.com>
2022-10-11 15:10:05 +0200

    Make median maps cover all depths by repeating This is convenient when overlaying other maps onto binner curvature maps

--- scripts/04_patch_flattening/03_median_projection.py
+++ scripts/04_patch_flattening/03_median_projection.py
@@ -27,7 +27,7 @@ OUTDIR = "/home/faruk/data2/DATA_MRI_NIFTI/derived/sub-01/median_maps"
 # Output directory
 if not os.path.exists(OUTDIR):
     os.makedirs(OUTDIR)
-    print("  Output directory: {}\n".format(OUTDIR))
+print("  Output directory: {}\n".format(OUTDIR))
 
 for i in range(len(MAPS)):
     # Determine output basename
@@ -40,6 +40,10 @@ for i in range(len(MAPS)):
 
     data = np.median(data, axis=2)
 
+    # Repeat to be able to overlay on 3D flat maps
+    data = np.repeat(data[:, :, None], nii.shape[2], axis=2)
+
+    # Save
     out = nb.Nifti1Image(data, header=nii.header, affine=nii.affine)
     nb.save(out, outname)
 
